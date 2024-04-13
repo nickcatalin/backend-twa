b21e94235dfe4005dd2f75f8e46d789d4539af58
nickcatalin <florin.nicola@stud.acs.upb.ro>
2024-04-13 05:31:01 +0300

    last commit

---
+++ src/main/java/com/example/backend/entites/User.java
@@ -0,0 +1,63 @@
+package com.example.backend.entites;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@Entity
+@Table(name = "app_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false)
+    @Size(max = 100)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    @Size(max = 100)
+    private String lastName;
+
+    @Column(nullable = false)
+    @Size(max = 100)
+    private String login;
+
+    @Column(nullable = false)
+    @Size(max = 100)
+    private String password;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "user")
+    private List<Link> links = new ArrayList<>();
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "user")
+    private List<Comment> comments = new ArrayList<>();
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "user")
+    private List<Rating> ratings = new ArrayList<>();
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "user")
+    private List<Bookmark> bookmarks = new ArrayList<>();
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles = new HashSet<>();
+
+}
---
+++ src/main/java/com/example/backend/mappers/UserMapper.java
@@ -0,0 +1,24 @@
+package com.example.backend.mappers;
+
+import com.example.backend.dtos.SignUpDto;
+import com.example.backend.dtos.UserDto;
+import com.example.backend.dtos.UserEditDto;
+import com.example.backend.entites.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+
+    UserDto toUserDto(User user);
+
+    @Mapping(target = "password", ignore = true)
+    User signUpToUser(SignUpDto signUpDto);
+
+
+
+    UserEditDto toUserEditDto(User user);
+
+    User userEditDtoToUser(UserEditDto userEditDto);
+
+}
